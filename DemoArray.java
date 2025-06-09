3863427b3f4125cde582200d739d1fefcb02ebc7
AshwiniDoble <ashiwinidoble2000@gmail.com>
2025-06-09 11:37:56 +0530

    Add files via upload

---
+++ DemoArray.java
@@ -0,0 +1,17 @@
+public class DemoArray {
+    public static void main(String[] args) {
+        //int nums[]={3,7,5,9}; //initialized Array
+
+        int nums[] =  new int[4];
+        nums[0]=3;
+        nums[1]=5;
+        nums[2]=8;
+        nums[3]=4;
+
+        for (int i=0; i<=3; i++)
+        {
+            System.out.println(nums[i]);
+        }
+        
+    }
+}
