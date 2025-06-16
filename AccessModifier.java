0ede4feefb94b7a678b547813a00fcf120cd9c36
AshwiniDoble <ashiwinidoble2000@gmail.com>
2025-06-16 09:39:28 +0530

    Add files via upload

---
+++ AccessModifier.java
@@ -0,0 +1,11 @@
+package tool;
+import other.Acess;
+
+import java.util.*;
+
+public class AccessModifier {
+    public static void main(String args[]) {
+        Acess obj = new Acess();
+        System.out.println(obj.marks);
+    }
+}
---
+++ DynamicDispatch.java
@@ -0,0 +1,39 @@
+package tool;
+
+class A
+{
+    public void show()
+    {
+        System.out.println("In A show");
+    }
+}
+
+class B extends A
+{
+    public void show()
+    {
+        System.out.println("In B show");
+    }
+
+}
+class C extends B
+{
+    public void show()
+    {
+        System.out.println("In C show");
+    }
+
+}
+
+public class DynamicDispatch {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.show();
+
+        obj = new B();
+        obj.show();
+
+        obj = new C();
+        obj.show();
+    }
+}
